5005fbca6bea6f4045a95633fa4f1898250eb1a6
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-31 05:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function createEmployeeRecord(array){
+  return{
+    firstName : array[0],
+    familyName : array[1],
+    title : array[2],
+    payPerHour : array[3],
+    timeInEvents : [],
+    timeOutEvents : []
+  }
+}
+function createEmployeeRecords(employeeRecords){
+  let newArr = employeeRecords.map(row => createEmployeeRecord(row))
+  return newArr
+}
+function createTimeInEvent() {
+
+}
